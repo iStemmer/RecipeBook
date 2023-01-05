8fd13424a1da370d8dc24ae882e9844bda2e66e0
Oleg Kartashev <officinator@gmail.com>
2023-01-05 21:27:52 +0300

    Подключение библиотек

--- src/main/java/com/madphysicist/recipebook/controllers/FirstController.java
+++ src/main/java/com/madphysicist/recipebook/controllers/FirstController.java
@@ -1,22 +1,25 @@
 package com.madphysicist.recipebook.controllers;
 
-import com.madphysicist.recipebook.RecipeBookApplication;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Пробный контролер", description = "Проверка работы и инфо приложения")
 public class FirstController {
     @GetMapping
-    public String appIsRunning(){
+    @Operation(description = "Проверка работы приложения")
+    public String appIsRunning() {
         return "Приложение запущено";
     }
+
     @GetMapping("/info")
-    public String appInfo(){
-        StringBuilder sb = new StringBuilder("Author name: ");
-        sb.append("Oleg Kartashev").append("<br>");
-        sb.append("Project name: RecipeBook").append("<br>");
-        sb.append("Creation date: 11/12/2022").append("<br>");
-        sb.append("Project description: My first spring project");
-        return sb.toString();
+    @Operation(description = "Информация о приложении")
+    public String appInfo() {
+        return "Author name: " + "Oleg Kartashev" + "<br>" +
+                "Project name: RecipeBook" + "<br>" +
+                "Creation date: 11/12/2022" + "<br>" +
+                "Project description: My first spring project";
     }
 }
